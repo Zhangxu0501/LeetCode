409833e2812f483578859df929ee9cf69da2cc9f
81248397 <81248397@qq.com>
2019-01-16 00:39:46 +0800

    update 2

---
+++ src/problem2_add_two/ListNode.java
@@ -0,0 +1,16 @@
+package problem2_add_two;
+
+/**
+ * @author zhangxu create on 2019/1/16
+ * version 1.0
+ */
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
