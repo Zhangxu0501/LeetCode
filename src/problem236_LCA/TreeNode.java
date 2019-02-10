91d1f1763914e7d26e19b777c056b16c5e41cfd6
81248397 <81248397@qq.com>
2019-02-10 18:48:55 +0800

    update

---
+++ src/problem236_LCA/TreeNode.java
@@ -0,0 +1,13 @@
+package problem236_LCA;
+
+/**
+ * @author zhangxu create on 2019/2/10
+ * version 1.0
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
